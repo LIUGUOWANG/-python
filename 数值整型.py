001135f1b0822f951b9c81e78fc87e1a3b1bec01
LIUGUOWANG <59330915+LIUGUOWANG@users.noreply.github.com>
2025-01-22 21:38:55 +0800

    Add files via upload

---
+++ 数值整型.py
@@ -0,0 +1,8 @@
+# 编写人：刘富贵 
+# 日期：2025-01-18 时间：19:02
+num1=0b101011
+num2=0o1174
+num3=0x1a2df
+print(num1)
+print(num2)
+print(num3)
---
+++ 数据类型之间的转换.py
@@ -0,0 +1,9 @@
+# 编写人：刘富贵 
+# 日期：2025-01-19 时间：18:04
+
+#x='3.14'
+#y=float(x)
+#print(int(eval(x)))
+print("请输入你的年龄：")
+age=input()
+print(age,type(eval(age)))
